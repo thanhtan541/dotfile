2be1545ae43dd39bd3c6a49de1d4e7b6ca9ec94e
tan <tt.nguyen@laudert.de>
2019-06-04 17:06:50 +0700

    [Initial] Bash script
    Why:
    * This is for developing shell script helper
    This changes addresses the need by:
    * Bash script

---
+++ bash/alias.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#
+# Directory shortcut
+alias contnetserv="cd $PROJECT_GIT/contentserv"
+alias todev="cd $PROJECT_GIT/.."
+alias toconfig="cd $PROJECT_GIT/../dev-config"
+#
+# Custom aliases for workspace
+alias cl='clear'
+alias sacl='tail -f /usr/local/var/log/httpd/access_log'
+alias indesignlog='tail -f ~/Desktop/idlog_cs11.log'
+alias secl='tail -f /usr/local/var/log/httpd/error_log'
+alias mysqlshell="mysql -h localhost -u root -p"
+#
+# Git stuffs
+alias gs='git status'
+alias gf='git fetch'
+alias gl='git log'
+alias gc='git commit'
---
+++ bash/droplist.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+select os in "macos" "window" "linux"
+do
+    echo "${os}"
+    break
+done
