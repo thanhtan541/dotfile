6ae45388cd78fc2fb483a24d39926f0a6ec562a3
thanhtan541 <thanhtan004100@gmail.com>
2022-04-03 12:24:56 +0700

    Add brightness control script

---
+++ bash/brightness_up.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+brightness=$(cat /sys/class/backlight/intel_backlight/brightness)
+brightness=$(expr $brightness + 10000)
+
+echo $brightness > /sys/class/backlight/intel_backlight/brightness
+
