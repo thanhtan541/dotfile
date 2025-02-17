cf6ed1eb01cfbd348990263515a24589803d369f
thanhtan541 <thanhtan004100@gmail.com>
2025-02-17 16:00:30 +0700

    feat: update bash

--- bash/shortcut.sh
+++ bash/shortcut.sh
@@ -12,10 +12,20 @@ function init_solana_local() {
     if [ -d "./tmp" ]; then
         echo "Setting up test-ledger in tmp directory"
 
-        tmux split-window -h 'solana-test-validator -l ./tmp/test-ledger' && \
+        tmux split-window -h 'solana-test-validator --reset -l ./tmp/test-ledger' && \
         tmux select-pane -t !
     else
         echo "Please create tmp directory"
     fi
 }
 
+# -n is force to open new instance
+# Fixme: find a way to check the current profile instance running
+# before using -n
+function chrome_default() {
+    open -n -a "Google Chrome" --args --profile-directory=Default
+}
+
+function chrome_tan() {
+    open -n -a "Google Chrome" --args --profile-directory="Profile 1"
+}
